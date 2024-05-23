8608cc989d3ffdc9f2183aa7a312a6570093460f
Varun6377 <varun.nellutla6@gmail.com>
2024-05-23 19:24:41 +0530

    updates

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 import express from "express";
-const app = express();
 import cors from "cors";
-import connectDB from "./server/db.js";
-import Salaries from "./server/modelSchema.js";
+import connectDB from "./db.js";
+import Salaries from "./modelSchema.js";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
- 
+
 connectDB();
 
+const app = express();
 app.use(cors());
 
 app.use("/api/salaries", async (_, res) => {
